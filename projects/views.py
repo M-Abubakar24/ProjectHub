cb7a6d8570708e197c2ec40df07aff290797b324
muhammad abubakar <muhammad.abubakar241996@gmail.com>
2026-08-31 17:23:51 +0500

    Implement role based task permissions

--- projects/views.py
+++ projects/views.py
@@ -2,16 +2,24 @@ from rest_framework import generics
 
 from .models import Project
 from .serializers import ProjectSerializer
-from accounts.permissions import IsProjectManager
+from accounts.permissions import IsProjectManager, IsEmployee
 
 
 class ProjectListCreateView(generics.ListCreateAPIView):
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
-    permission_classes = [IsProjectManager]
+
+    def get_permissions(self):
+        if self.request.method == "POST":
+            return [IsProjectManager()]
+        return [IsEmployee()]
 
 
 class ProjectDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
-    permission_classes = [IsProjectManager]
+
+    def get_permissions(self):
+        if self.request.method in ["PUT", "PATCH", "DELETE"]:
+            return [IsProjectManager()]
+        return [IsEmployee()]
--- tasks/views.py
+++ tasks/views.py
@@ -2,16 +2,24 @@ from rest_framework import generics
 
 from .models import Task
 from .serializers import TaskSerializer
-from accounts.permissions import IsProjectManager
+from accounts.permissions import IsProjectManager, IsEmployee
 
 
 class TaskListCreateView(generics.ListCreateAPIView):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
-    permission_classes = [IsProjectManager]
+
+    def get_permissions(self):
+        if self.request.method == "POST":
+            return [IsProjectManager()]
+        return [IsEmployee()]
 
 
 class TaskDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
-    permission_classes = [IsProjectManager]
+
+    def get_permissions(self):
+        if self.request.method in ["PUT", "PATCH", "DELETE"]:
+            return [IsProjectManager()]
+        return [IsEmployee()]
