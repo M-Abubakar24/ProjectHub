e9e92dd8082bf691d3e1e07e6e516253c793c783
muhammad abubakar <muhammad.abubakar241996@gmail.com>
2026-08-31 17:41:58 +0500

    Initial ProjectHub implementation

--- accounts/permissions.py
+++ accounts/permissions.py
@@ -27,3 +27,15 @@ class IsEmployee(BasePermission):
                 "EMPLOYEE",
             ]
         )
+class IsTaskOwnerOrManager(BasePermission):
+
+    def has_permission(self, request, view):
+        return request.user.is_authenticated
+
+    def has_object_permission(self, request, view, obj):
+        # Admin and Project Manager have full access
+        if request.user.role in ["ADMIN", "PROJECT_MANAGER"]:
+            return True
+
+        # Employee can only access their own assigned task
+        return obj.assigned_to == request.user
--- tasks/views.py
+++ tasks/views.py
@@ -2,24 +2,46 @@ from rest_framework import generics
 
 from .models import Task
 from .serializers import TaskSerializer
-from accounts.permissions import IsProjectManager, IsEmployee
+from accounts.permissions import (
+    IsProjectManager,
+    IsEmployee,
+    IsTaskOwnerOrManager,
+)
 
 
 class TaskListCreateView(generics.ListCreateAPIView):
-    queryset = Task.objects.all()
+
     serializer_class = TaskSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+
+        if user.role in ["ADMIN", "PROJECT_MANAGER"]:
+            return Task.objects.all()
+
+        return Task.objects.filter(assigned_to=user)
 
     def get_permissions(self):
         if self.request.method == "POST":
             return [IsProjectManager()]
+
         return [IsEmployee()]
 
 
 class TaskDetailView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Task.objects.all()
+
     serializer_class = TaskSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+
+        if user.role in ["ADMIN", "PROJECT_MANAGER"]:
+            return Task.objects.all()
+
+        return Task.objects.filter(assigned_to=user)
 
     def get_permissions(self):
         if self.request.method in ["PUT", "PATCH", "DELETE"]:
-            return [IsProjectManager()]
+            return [IsTaskOwnerOrManager()]
+
         return [IsEmployee()]
